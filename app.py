7919289069ae1168612cbfcfd5d0d7aafea6abde
polyakovn <polyakovn@carleton.edu>
2018-10-22 16:36:16 -0400

    refactoring

--- app.py
+++ app.py
@@ -4,121 +4,198 @@ from google.transit import gtfs_realtime_pb2
 from protobuf_to_dict import protobuf_to_dict
 from orderedset import OrderedSet
 
-app = Flask(__name__)
-stop_dict = {}
-stop_names = []
-api_key = '75b4f38677044eca8103ef4bea7cd386'
+class MTA():
 
-def initialize_stop_names_and_stop_dict():
-    global stop_names, stop_dict
-    with open('static/Stations.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            station_id = row[2]
-            station_name = row[5]
-            station_lines = row[7]
-            stop_names.append(station_name)
-            stop_dict[station_id] = (station_name, station_lines)
-    stop_names = OrderedSet(sorted(stop_names))
+    def __init__(self):
+        self.stations = []
+        self.station_names = []
 
-initialize_stop_names_and_stop_dict()
+    def make_station_list(self):
+        with open('static/Stations.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                station_name = row[5]
+                station_lines = row[7].split(" ")
+                station_id = row[2]
+                self.add_or_modify_station(station_name, station_lines, station_id) #some station names have multiple ids/lines
 
-def get_next_arrivals(train_feed, station_id):
-    train_dict = {}
-    south_id = station_id + "S"
-    north_id = station_id + "N"
-    for train in train_feed:
-        if train.get('trip_update', False) != False:
-            train_arrivals = train['trip_update']['stop_time_update']
-            train_line = train['trip_update']['trip']['route_id']
-            for arrival in train_arrivals:
-                stop_id = arrival['stop_id']
-                if stop_id == south_id or stop_id == north_id:
-                    add_arrival_to_train_dict(arrival, train_line, stop_id, train_dict)
-    print(train_dict)
-    return train_dict
+    def add_or_modify_station(self, station_name, station_lines, station_id):
+        for station in self.stations:
+            if station.name == station_name: #if a station with this name already exists, modify it
+                station.add_lines(station_lines)
+                station.add_id(station_id)
+                return
+        station = Station(station_name, station_lines, station_id)
+        self.stations.append(station)
 
-def add_arrival_to_train_dict(arrival, train_line, stop_id, train_dict):
-    arrival_time = arrival['arrival']['time']
-    if arrival_time != None:
-        time_from_now = get_minutes_from_now(arrival_time)
-        if time_from_now > 0:
-            stop_line = stop_id + " " + train_line
-            if stop_line not in train_dict:
-                train_dict[stop_line] = [time_from_now]
-            else:
-                train_dict[stop_line].append(time_from_now)
+    def make_station_names(self):
+        f = open("static/stop_names.txt")
+        content = f.readlines()
+        self.station_names = [line.strip() for line in content]
 
-def get_minutes_from_now(epoch_time):
-    current_time = int(time.time())
-    time_until_train = int(int(epoch_time - current_time) / 60)
-    return time_until_train
+    def get_station(self, name):
+        for station in self.stations:
+            if name == station.name:
+                return station
 
-@app.route('/')
-def setup_page():
-    return render_template("home.html", stop_names = stop_names)
-
-@app.route('/line_station_info')
-def filter_results_by_line():
-    station_name = request.args['station']
-    line_name = request.args['line']
-    train_dict = get_station_info(station_name)
-    for train in train_dict.copy():
-        line = train.split(" ")[1]
-        if line != line_name:
-            del train_dict[train]
-    return jsonify(train_dict)
-
-@app.route('/station_info')
-def get_station_info(station_name=None):
-    if not station_name:
-        station_name = request.args['station']
-        return_all = True
-    else:
-        return_all = False
-    station_ids = get_station_ids(station_name)
-    all_arrivals = {}
-    for station_id in station_ids:
-        train_feeds = get_train_feeds(station_id)
+    def get_station_arrivals(self, station_name):
+        api_key = '75b4f38677044eca8103ef4bea7cd386'
+        all_arrivals = []
+        station = self.get_station(station_name)
+        train_feeds = station.get_train_feeds()
         for train_feed in train_feeds:
             feed = gtfs_realtime_pb2.FeedMessage()
             response = requests.get('http://datamine.mta.info/mta_esi.php?key=' + api_key + '&feed_id=' + str(train_feed))
             feed.ParseFromString(response.content)
             subway_feed = protobuf_to_dict(feed)
             realtime_data = subway_feed['entity']
-            next_arrivals = get_next_arrivals(realtime_data, station_id) #returns train dict
-            all_arrivals = {**all_arrivals, **next_arrivals}
-    if return_all:
-        return jsonify(all_arrivals)
-    else:
-        return all_arrivals
+            self.get_next_arrivals(realtime_data, station)
+        return station.next_arrivals
 
-def get_station_ids(station_name):
-    key_list = []
-    for key in stop_dict:
-        if stop_dict[key][0] == station_name:
-            key_list.append(key)
-    return key_list
 
-def get_train_feeds(station_id):
-    station_lines = set(stop_dict[station_id][1])
-    train_feeds = []
-    if station_lines.intersection(set(['1','2','3','4','5','6','S'])):
-        train_feeds.append(1)
-    if station_lines.intersection(set(['A','C','E','H','S'])):
-        train_feeds.append(26)
-    if station_lines.intersection(set(['N','Q','R','W'])):
-        train_feeds.append(16)
-    if station_lines.intersection(set(['B','D','F','M'])):
-        train_feeds.append(21)
-    if station_lines.intersection(set(['L'])):
-        train_feeds.append(2)
-    if station_lines.intersection(set(['SIR'])):
-        train_feeds.append(11)
-    if station_lines.intersection(set(['G'])):
-        train_feeds.append(31)
-    if station_lines.intersection(set(['J','Z'])):
-        train_feeds.append(36)
-    if station_lines.intersection(set(['7'])):
-        train_feeds.append(51)
-    return train_feeds
+    def get_next_arrivals(self, train_feed, station):
+        id_options = self.get_id_possibilities(station)
+        for train in train_feed:
+            if train.get('trip_update', False) != False:
+                train_arrivals = train['trip_update']['stop_time_update']
+                train_line = train['trip_update']['trip']['route_id']
+                for arrival in train_arrivals:
+                    stop_id = arrival['stop_id']
+                    arrival_time = arrival['arrival']['time']
+                    if stop_id in id_options and arrival_time != None:
+                        arrival_time = self.get_minutes_from_now(arrival_time)
+                        if arrival_time > 0:
+                            self.add_or_modify_arrival(station, train_line, stop_id, arrival_time)
+
+    def add_or_modify_arrival(self, station, train_line, stop_id, arrival_time):
+        for arrival in station.next_arrivals:
+            if arrival.line == train_line and arrival.direction == stop_id[-1]:
+                arrival.arrival_times.append(arrival_time)
+                return
+        arrival = Arrival(train_line, stop_id, arrival_time)
+        station.add_arrival(arrival)
+
+    def get_minutes_from_now(self, epoch_time):
+        current_time = int(time.time())
+        time_until_train = int(int(epoch_time - current_time) / 60)
+        return time_until_train
+
+    def get_id_possibilities(self, station):
+        id_list = []
+        for station_id in station.ids:
+            id_list.append(station_id + "S")
+            id_list.append(station_id + "N")
+        return id_list
+
+class Station():
+
+    def __init__(self, name, lines, station_id):
+        self.name = name
+        self.lines = lines
+        self.ids = [station_id]
+        self.next_arrivals = []
+
+    def add_lines(self, new_lines):
+        self.lines.extend(new_lines)
+
+    def add_id(self, station_id):
+        self.ids.append(station_id)
+
+    def add_arrival(self, arrival):
+        self.next_arrivals.append(arrival)
+
+    def get_train_feeds(self):
+        station_lines = set(self.lines)
+        train_feeds = []
+        if station_lines.intersection(set(['1','2','3','4','5','6','S'])):
+            train_feeds.append(1)
+        if station_lines.intersection(set(['A','C','E','H','S'])):
+            train_feeds.append(26)
+        if station_lines.intersection(set(['N','Q','R','W'])):
+            train_feeds.append(16)
+        if station_lines.intersection(set(['B','D','F','M'])):
+            train_feeds.append(21)
+        if station_lines.intersection(set(['L'])):
+            train_feeds.append(2)
+        if station_lines.intersection(set(['SIR'])):
+            train_feeds.append(11)
+        if station_lines.intersection(set(['G'])):
+            train_feeds.append(31)
+        if station_lines.intersection(set(['J','Z'])):
+            train_feeds.append(36)
+        if station_lines.intersection(set(['7'])):
+            train_feeds.append(51)
+        return train_feeds
+
+
+class Arrival():
+
+    def __init__(self, line, stop_id, arrival_time):
+        self.line = line
+        self.direction = stop_id[-1]
+        self.arrival_times = [arrival_time]
+
+app = Flask(__name__)
+mta = MTA()
+mta.make_station_list()
+mta.make_station_names()
+stop_names = mta.station_names
+
+def turn_to_json(arrivals_list):
+    arrival_string = '['
+    for arrival in arrivals_list:
+        arrival_times_string = '['
+        for time in arrival.arrival_times:
+            arrival_times_string += '"' + str(time) + '",'
+        arrival_times_string = arrival_times_string[:-1]
+        arrival_times_string += ']'
+        arrival_string += '{"line":"' + arrival.line + '", "direction":"' + arrival.direction + '", "arrival_times":' + arrival_times_string + "},"
+    arrival_string = arrival_string[:-1]
+    arrival_string += "]"
+    return arrival_string
+
+
+@app.route('/')
+def setup_page():
+    return render_template("home.html", stop_names = stop_names)
+
+@app.route('/station_info')
+def get_station_info(station_name=None):
+    if station_name == None:
+        station_name = request.args['station']
+    arrivals_list = mta.get_station_arrivals(station_name)
+    return turn_to_json(arrivals_list)
+
+@app.route('/filter_by_line')
+def add_line_filter():
+    station_name = request.args['station']
+    line_filter = request.args['line']
+    arrivals_list = mta.get_station_arrivals(station_name)
+    filtered_arrivals = []
+    for arrival in arrivals_list:
+        if arrival.line == line_filter:
+            filtered_arrivals.append(arrival)
+    return turn_to_json(filtered_arrivals)
+
+@app.route('/filter_by_direction')
+def add_direction_filter():
+    station_name = request.args['station']
+    direction_filter = request.args['direction']
+    arrivals_list = mta.get_station_arrivals(station_name)
+    filtered_arrivals = []
+    for arrival in arrivals_list:
+        if arrival.direction == direction_filter:
+            filtered_arrivals.append(arrival)
+    return turn_to_json(filtered_arrivals)
+
+@app.route('/filter_by_line_and_direction')
+def add_line_and_direction_filter():
+    station_name = request.args['station']
+    direction_filter = request.args['direction']
+    line_filter = request.args['line']
+    arrivals_list = mta.get_station_arrivals(station_name)
+    filtered_arrivals = []
+    for arrival in arrivals_list:
+        if arrival.direction == direction_filter and arrival.line == line_filter:
+            filtered_arrivals.append(arrival)
+    return turn_to_json(filtered_arrivals)
--- static/trains.js
+++ static/trains.js
@@ -1,39 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
   $('#line_selector').hide();
+  $('#direction_selector').hide();
   var dt = new Date();
   var cur_time = dt.toLocaleTimeString();
   $('#current_time').text("It is currently " + cur_time);
 }, false);
 
 function get_station_info() {
-  $('#line_selector').find('option').remove().end().append('<option>--Line--</option>');
-  var station = document.getElementById("station");
-  var selected_station = station.options[station.selectedIndex].text;
-  $.get('/station_info', {'station':selected_station} , function(train_list) {
-    var line_set = make_table(train_list);
-    add_line_options(line_set);
-    $('#line_selector').show();
+  var selected_station = $('#station').find(":selected").text();
+  $.get('/station_info', {'station':selected_station} , function(train_info) {
+    var trains = JSON.parse(train_info);
+    display_results(trains);
   });
 }
 
-function make_table(train_list) {
+function display_results(trains) {
   $('#times tr').remove();
   $('#times').append('<tr><th>Line</th><th>Direction</th><th>Next Arrivals (in minutes from now)</th></tr>')
-  var line_set = new Set();
-  for(train in train_list) {
-    let key = train.split(" ");
-    let line = key[1];
-    let dir = key[0].slice(-1);
-    let direction = dir == 'S' ? 'South' : 'North';
-    let arrival_times = train_list[train].slice(0,3);
-    add_station_row(line, direction, arrival_times);
-    line_set.add(line);
+  for(train in trains) {
+    add_station_row(trains[train].line, trains[train].direction, trains[train].arrival_times);
   }
-  return line_set;
+  display_line_filter(trains);
+  $("#direction_selector").show();
 }
 
-function add_line_options(line_set) {
-  for (let line of line_set.keys()) {
+function display_line_filter(trains) {
+  if ($('#line_selector').find(":selected").text() == '--Line--'){
+    $('#line_selector').find('option').remove().end().append('<option>--Line--</option>')
+    var station_lines = get_line_options(trains);
+    add_line_options(station_lines);
+  }
+  $("#line_selector").show();
+}
+
+function get_line_options(trains) {
+  station_lines = new Set();
+  for(train in trains) {
+    station_lines.add(trains[train].line);
+  }
+  return station_lines;
+}
+
+function add_line_options(station_lines) {
+  for (let line of station_lines.keys()) {
     $("#line_selector").append('<option>' + line + '</option>');
   }
 }
@@ -46,21 +55,42 @@ function add_station_row(line, direction, arrival_times) {
   var times_cell = row.insertCell(2);
   line_cell.innerHTML = line;
   direction_cell.innerHTML = direction;
-  times_cell.innerHTML = arrival_times;
+  times_cell.innerHTML = arrival_times.slice(0,3);
 }
 
-function filter_by_line(){
-  var station = document.getElementById("station");
-  var selected_station = station.options[station.selectedIndex].text;
-  var lines = document.getElementById("line_selector");
-  var line = lines.options[lines.selectedIndex].text;
-  if(line != '--Line--'){
-    $.get('/line_station_info', {'station':selected_station, 'line': line}, function(train_list) {
-      make_table(train_list);
-    });
+function add_filter(){
+  var station = $('#station').find(":selected").text();
+  var line = $('#line_selector').find(":selected").text();
+  var direction= $('#direction_selector').find(":selected").text();
+  if(line != '--Line--' && direction != '--Direction--'){
+    filter_by_line_and_direction(station, line, direction);
+  } else if (line != '--Line--') {
+    filter_by_line(station, line);
+  } else if (direction != '--Direction--') {
+    filter_by_direction(station, direction);
   } else {
-    $.get('/station_info', {'station':selected_station}, function(train_list) {
-      make_table(train_list);
-    });
+    get_station_info();
   }
+}
+
+function filter_by_line_and_direction(station, line, direction){
+  $.get('/filter_by_line_and_direction', {'station':station, 'line':line, 'direction':direction}, function(train_info) {
+    var trains = JSON.parse(train_info);
+    display_results(trains);
+  });
+}
+
+
+function filter_by_line(station, line){
+  $.get('/filter_by_line', {'station':station, 'line':line}, function(train_info) {
+    var trains = JSON.parse(train_info);
+    display_results(trains);
+  });
+}
+
+function filter_by_direction(station, direction){
+  $.get('/filter_by_direction', {'station':station, 'direction':direction}, function(train_info) {
+    var trains = JSON.parse(train_info);
+    display_results(trains);
+  });
 }
